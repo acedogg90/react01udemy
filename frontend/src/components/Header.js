fe8e4980b96c78e61b3bb30de8a9202e47bd0771
Allen Monroe <admonroe@gmail.com>
2024-02-09 22:22:32 -0600

    basic setup, footer and header components plus bootstrap

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Header() {
+  return (
+    <header>
+        <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
+      <Container fluid>
+        <Navbar.Brand href="/">ProShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="mr-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+            <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+          </Nav>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </header>
+  )
+}
+
+export default Header
